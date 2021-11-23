71dd1c841646797fbadc6930eb25f23ca97ec027
Day-bi <nuripeace328@naver.com>
2021-11-23 17:14:00 +0900

    년도별 정리

--- safe_code.R
+++ safe_code.R
@@ -1,15 +1,10 @@
 # 체감 안전도
-
-#
 setwd("C:/Users/daybi/Documents/Titano/competi/DataMapstory(211119)/체감안전도")
 
+# 불러들이기
 library(XML)
-
-#
 list.files()
 file_list <- list.files()
-file_list
-
 
 safe <- NULL
 for (i in 1:length(file_list)){
@@ -18,6 +13,17 @@ for (i in 1:length(file_list)){
   cat("\n", i)
 }
 
-names(safe) <- c("TRGET_YM","POLC_NM","SURVEY_TARGET_CNT_TOTAL","SURVEY_TARGET_CNT_AREA1","SURVEY_TARGET_CNT_AREA2","SURVEY_TARGET_CNT_AREA3","SURVEY_TARGET_CNT_MALE","SURVEY_TARGET_CNT_FEMALE","SURVEY_TARGET_CNT_AGE_20_LESS","SURVEY_TARGET_CNT_AGE_30","SURVEY_TARGET_CNT_AGE_40","SURVEY_TARGET_CNT_AGE_50","SURVEY_TARGET_CNT_AGE_60_OVER","GNRLZ_SENS_SAFECHK_SCORE","GNRL_SENS_SAFECHK_SCORE","FIELD_SAFECHK_SCORE","CRIME_SAFECHK_SCORE","TRNSPORT_INCI_SAFECHK_SCORE","LAW_ORDER_COMP_SCORE","THEFT_VIOLENCE_SAFECHK_SCORE","ROBBER_KILL_SAFECHK_SCORE","GNRL_POLC_EFFORT_SCORE")
+# 정제
+library(dplyr)
+library(stringr)
+library(tidyr)
 
-#
+#컬럼 정리
+safe = safe[,-c(4,5,6)] 
+
+#labeling
+names(safe) <- c("TRGET_YM","POLC_NM","SURVEY_TARGET_CNT_TOTAL","SURVEY_TARGET_CNT_MALE","SURVEY_TARGET_CNT_FEMALE","SURVEY_TARGET_CNT_AGE_20_LESS","SURVEY_TARGET_CNT_AGE_30","SURVEY_TARGET_CNT_AGE_40","SURVEY_TARGET_CNT_AGE_50","SURVEY_TARGET_CNT_AGE_60_OVER","GNRLZ_SENS_SAFECHK_SCORE","GNRL_SENS_SAFECHK_SCORE","FIELD_SAFECHK_SCORE","CRIME_SAFECHK_SCORE","TRNSPORT_INCI_SAFECHK_SCORE","LAW_ORDER_COMP_SCORE","THEFT_VIOLENCE_SAFECHK_SCORE","ROBBER_KILL_SAFECHK_SCORE","GNRL_POLC_EFFORT_SCORE")
+names(safe) <- c("년월","경찰서","설문대상인원_전체","설문대상인원_남","설문대상인원_여","설문대상인원_20대이하","설문대상인원_30대","설문대상인원_40","설문대상인원_50대","설문대상인원_60대","종합체감안전도_점수","전반적안전도_점수","분야별안전도_점수","범죄안전도_점수","교통사고안전도_점수","법질서준수도_점수","절도폭력안전도_점수","강도살인안전도_점수","전반적경찰노력도_점수")
+
+#년도별 정리
+safe$년월 <- substr(safe$년월,1,4) 
